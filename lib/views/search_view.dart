59baf02a047a89aa0eadbffb21c09cbf91153c93
Mustafa <mm0118250@gmail.com>
2024-07-06 21:29:40 +0300

    create text field

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -7,13 +7,35 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text(
           'Search a City',
           style: TextStyle(
             color: Colors.white,
           ),
         ),
         backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Search',
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+              hintText: 'Enter City Name',
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: const BorderSide(color: Colors.green)),
+            ),
+          ),
+        ),
       ),
     );
   }

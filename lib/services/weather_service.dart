a78944888bbb8254994091a46ed0628c945a3816
Mustafa <mm0118250@gmail.com>
2024-07-07 14:17:19 +0300

    Handling Exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,10 +7,14 @@ class WeatherService {
   WeatherService(this.dio);
   String apiKey = "2967cbd44ab444cb937185746233007";
   final String baseUrl = "http://api.weatherapi.com/v1";
-  Future<WeatherModel> getWeatherService({required String cityName}) async {
-    Response response =
-        await dio.get("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1");
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getWeatherService({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1");
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } catch (e) {
+      return null;
+    }
   }
 }

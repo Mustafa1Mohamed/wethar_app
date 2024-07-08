28e5b8d830f479bf74bdebf8cd4db751e02fda94
Mustafa <mm0118250@gmail.com>
2024-07-08 15:40:46 +0300

    Provide cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:wethar_app/views/search_view.dart';
 import 'package:wethar_app/widgets/weather_info_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
